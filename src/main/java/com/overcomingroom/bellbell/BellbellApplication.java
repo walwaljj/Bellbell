f0c64ef65d65797853b108a1fb783c70b901c688
Shjung <wjdtngus9@gmail.com>
2024-02-21 04:00:08 +0900

    🎉 setting : Initial Commit

---
+++ src/main/java/com/overcomingroom/bellbell/BellbellApplication.java
@@ -0,0 +1,13 @@
+package com.overcomingroom.bellbell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellbellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BellbellApplication.class, args);
+	}
+
+}
