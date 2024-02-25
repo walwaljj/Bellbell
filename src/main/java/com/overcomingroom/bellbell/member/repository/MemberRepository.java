ee91d7430ec72fe35a9c3ed59aa77174a3af54f0
Ingyu Song <ingyu3879@gmail.com>
2024-02-25 17:30:38 +0900

    :recycle: refactor: MemberRepository 수정

--- src/main/java/com/overcomingroom/bellbell/member/repository/MemberRepository.java
+++ src/main/java/com/overcomingroom/bellbell/member/repository/MemberRepository.java
@@ -3,6 +3,6 @@ package com.overcomingroom.bellbell.member.repository;
 import com.overcomingroom.bellbell.member.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Long, Member> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
 }
