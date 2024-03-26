82b01a096d8c1a19b63b2273b9d4a5ee2e27f22f
Shjung <wjdtngus9@gmail.com>
2024-03-26 20:33:59 +0900

    :sparkles: feat :  basicNotification service 클래스 생성

---
+++ src/main/java/com/overcomingroom/bellbell/basicNotification/service/BasicNotificationService.java
@@ -0,0 +1,26 @@
+package com.overcomingroom.bellbell.basicNotification.service;
+
+import com.overcomingroom.bellbell.basicNotification.domain.dto.BasicNotificationRequestDto;
+import com.overcomingroom.bellbell.basicNotification.domain.entity.BasicNotification;
+import com.overcomingroom.bellbell.basicNotification.repository.BasicNotificationRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 사용자 알림 서비스를 제공하는 클래스입니다.
+ */
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class BasicNotificationService {
+
+    private final BasicNotificationRepository basicNotificationRepository;
+
+    public BasicNotification setNotification(BasicNotificationRequestDto basicNotificationRequestDto) {
+        BasicNotification basicNotification = BasicNotification.toEntity(basicNotificationRequestDto);
+        return basicNotificationRepository.save(basicNotification);
+    }
+}
