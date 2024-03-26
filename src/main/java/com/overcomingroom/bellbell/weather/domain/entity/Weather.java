7aceb11627da65dcf65e7929c23f8be8beb1e6de
Shjung <wjdtngus9@gmail.com>
2024-03-26 20:34:13 +0900

    :sparkles: feat :  basicNotification 컬럼 추가

--- src/main/java/com/overcomingroom/bellbell/weather/domain/entity/Weather.java
+++ src/main/java/com/overcomingroom/bellbell/weather/domain/entity/Weather.java
@@ -1,10 +1,8 @@
 package com.overcomingroom.bellbell.weather.domain.entity;
 
+import com.overcomingroom.bellbell.basicNotification.domain.entity.BasicNotification;
 import com.overcomingroom.bellbell.weather.domain.dto.WeatherDto;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @NoArgsConstructor
@@ -18,15 +16,18 @@ public class Weather {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long memberId;
-    @Setter
-    private Long basicNotificationId;
     private String address;
     private String gridX;
     private String gridY;
 
+    @OneToOne
+    @JoinColumn(name = "basic_notification_id")
+    @Setter
+    private BasicNotification basicNotification;
+
     public static Weather toEntity(WeatherDto weatherDto) {
         return Weather.builder()
-                .basicNotificationId(weatherDto.getBasicNotificationId())
+                .basicNotification(weatherDto.getBasicNotification())
                 .memberId(weatherDto.getMemberId())
                 .address(weatherDto.getAddress())
                 .gridX(weatherDto.getGridX())
