6893b1fca6cd39d1ffb7fb4af5bf379b898265f8
Shjung <wjdtngus9@gmail.com>
2024-03-29 23:20:48 +0900

    :sparkles: feat : resolver 추가

--- src/main/java/com/overcomingroom/bellbell/config/CorsConfig.java
+++ src/main/java/com/overcomingroom/bellbell/config/CorsConfig.java
@@ -1,11 +1,15 @@
 package com.overcomingroom.bellbell.config;
 
 import com.overcomingroom.bellbell.interceptor.AuthorizationInterceptor;
+import com.overcomingroom.bellbell.resolver.LoginUserResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
  * CORS(Cross-Origin Resource Sharing) 설정을 구성하는 클래스입니다.
@@ -15,6 +19,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     private final AuthorizationInterceptor authorizationInterceptor;
+    private final LoginUserResolver loginUserResolver;
 
     /**
      * CORS(Cross-Origin Resource Sharing) 설정을 추가합니다.
@@ -33,6 +38,14 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(this.authorizationInterceptor);
+        registry.addInterceptor(this.authorizationInterceptor)
+                .addPathPatterns("/**") // 모든 요청은 인터셉터를 거치도록 함.
+                .excludePathPatterns("/v1/login") // 로그인 시 AccessToken이 없기 때문에 해당 요청만 제외
+                .excludePathPatterns("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html"); // 스웨거 예외 처리
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserResolver);
     }
 }
